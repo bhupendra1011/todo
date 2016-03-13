4589d092742d5410e7ad35fc75dc0441c0d061d4
bhupendra-negi <bhupendra.negi1011@gmail.com>
2016-03-13 21:18:25 +0530

    Add post for todos

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 // basic server file
 var express = require("express");
+
+//body parser module
+var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// attach middleware to express module so that it parses everyting we get POST data
+app.use(bodyParser.json());
+
 //create collection of models for data purpose
-var todos = [{
+// below data would br craeted by POST method
+/*var todos = [{
   id: 1,
-  description: "Go for haircut",
+  description: "Go for haircut and trimming",
   completed: false
 }, {
   id: 2,
@@ -17,6 +24,23 @@ var todos = [{
   description: "Go to gym",
   completed: true
 }];
+*/
+
+var todos = [];
+var todoNextId = 1;
+
+// POST method to add data
+app.post('/todos',function(req,res){
+  var body = req.body;
+  console.log("description :" + body.description);
+  body.id = todoNextId;
+  todos.push(body);
+  todoNextId++;
+  res.json("post data pushed");
+});
+
+
+
 
 // different request for todos task listen
 // GET todos/
