31e0434d2391357f5b77655f976793eccd78a3df
bhupendra-negi <bhupendra.negi1011@gmail.com>
2016-03-14 00:44:32 +0530

    refactored code with underscore.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var express = require("express");
 
 //body parser module
 var bodyParser = require("body-parser");
+
+//underscore module
+var _ = require("underscore");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -30,9 +33,16 @@ var todos = [];
 var todoNextId = 1;
 
 // POST method to add data
-app.post('/todos',function(req,res){
+app.post('/todos', function(req, res) {
   var body = req.body;
-  console.log("description :" + body.description);
+  //check for only two data fields provided
+  var filteredObj = _.pick(body,"description","completed");
+  // check for validations for todoNextId
+  if (!filteredObj || body.description.trim().length === 0 || !_.isBoolean(body.completed) || !_.isString(body.description)) {
+    return res.status(400).send("Invalid data submitted");
+  }
+  // trim body description
+  body.description = body.description.trim();
   body.id = todoNextId;
   todos.push(body);
   todoNextId++;
@@ -50,16 +60,25 @@ app.get('/todos', function(req, res) {
 })
 
 // GET todos/:id
-app.get("/todos/:id",function(req,res) {
-  var todoId = parseInt(req.params.id,10);
+app.get("/todos/:id", function(req, res) {
+  var todoId = parseInt(req.params.id, 10);
   //res.send("You are asking for Item : " + todoId );
-  var flag  = 0;
+  var obj = _.findWhere(todos, {
+    id: todoId
+  })
+  if (obj) {
+    res.json(obj);
+  } else {
+    res.status(404).send("to do task with given ID not found")
+  }
+  /*var flag  = 0;
   for(var i = 0;i<todos.length;i++)
   {
     var obj = todos[i];
     if (obj.id === todoId) { res.json(obj);flag=1}
   };
    if (!flag) res.status(404).send("to do task not found"); //s end 404 is not in list
+   */
 })
 
 app.get('/', function(req, res) {
