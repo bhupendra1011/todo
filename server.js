edea332ee8c1a2571b04bcabc09e76f2828aaf07
bhupendra-negi <bhupendra.negi1011@gmail.com>
2016-03-14 23:10:28 +0530

    added PUT DELETE http

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ var todoNextId = 1;
 app.post('/todos', function(req, res) {
   var body = req.body;
   //check for only two data fields provided
-  var filteredObj = _.pick(body,"description","completed");
+  var filteredObj = _.pick(body, "description", "completed");
   // check for validations for todoNextId
   if (!filteredObj || body.description.trim().length === 0 || !_.isBoolean(body.completed) || !_.isString(body.description)) {
     return res.status(400).send("Invalid data submitted");
@@ -58,6 +58,58 @@ app.get('/todos', function(req, res) {
   // return json data to request
   res.json(todos);
 })
+
+// delete request
+app.delete("/todos/:id", function(req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  //res.send("You are asking for Item : " + todoId );
+  var obj = _.findWhere(todos, {
+    id: todoId
+  })
+
+  if (obj) {
+    todos = _.without(todos, obj);
+
+    res.send("record removed for id :" + todoId);
+  } else {
+    res.status(404).json({
+      "error": "such record not exist"
+    })
+  }
+
+});
+
+//update request  PUT
+app.put("/todos/:id", function(req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var obj = _.findWhere(todos, {
+    id: todoId
+  });
+  if (obj) {
+    var validAttributes = {}
+
+    //check for only two data fields provided
+    var body = _.pick(req.body, "description", "completed");
+    if (body.hasOwnProperty("completed") && _.isBoolean(body.completed)) {
+      validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty("completed")) return res.status(400).send("data not in correct format")
+    if (body.hasOwnProperty("description") && _isString(body.description) && body.description.trim().length > 0) {
+      validAttributes.description = body.description;
+
+    } else if (body.hasOwnProperty("description")) return res.status(400).send("data not in correct format");
+    // here valid attributes are there
+    _.extend(obj, validAttributes);
+    res.send("updated record");
+  } else {
+    return res.status(404).json({
+      "error": "record does not exist"
+    });
+  }
+
+
+});
+
+
 
 // GET todos/:id
 app.get("/todos/:id", function(req, res) {
